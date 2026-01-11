c146207b593d3b956d6d989d2d60db12d235df06
Maynard Peralta <maynperalta@gmail.com>
2026-01-10 19:17:59 -0700

    Added methods to add, borrow, return, print, and search books.

--- CSC372Module8/src/main/Main.java
+++ CSC372Module8/src/main/Main.java
@@ -6,32 +6,67 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
-		int userChoice = scnr.nextInt();
+		Inventory bookInventory = new Inventory();
+		int userChoice = 0;
+// Switch statement menu with try/catch block for error handling		
+		while (userChoice != 6) {
+			try {
+				System.out.println("Welcome to the library program. Please make a selection: ");
+				System.out.println("1. Add Book");
+				System.out.println("2. Borrow Book");
+				System.out.println("3. Return Book");
+				System.out.println("4. Print All Books");
+				System.out.println("5. Search Book By Title");
+				System.out.println("6. Exit");
+				
+				userChoice = scnr.nextInt();
+				scnr.nextLine();
 		
-		System.out.println("Welcome to the library program. Please make a selection: "
-				+ "1. Add Book"
-				+ "2. Borrow Book"
-				+ "3. Return Book"
-				+ "4. Print All Books"
-				+ "5. Search Book By Title"
-				+ "6. Exit");
-		
-		switch(userChoice) {
-		case 1:
-			//TODO Enter addBook method
-		case 2:
-			//TODO Enter borrowBook method
-		case 3:
-			//TODO enter returnBook method
-		case 4:
-			//TODO enter printAll method
-		case 5:
-			//TODO enter searchByTitle method
-		case 6:
-			System.out.println("Exiting program. Goodbye.");
-			break;
-		default: 
-			System.out.println("Please make a valid selection (1 - 6).");
+				switch(userChoice) {
+					case 1:
+						System.out.println("Enter book ID: ");
+						int id = scnr.nextInt();
+						scnr.nextLine();
+						
+						System.out.println("Enter book title: ");
+						String title = scnr.nextLine();
+						
+						System.out.println("Enter book author: ");
+						String author = scnr.nextLine();
+						
+						System.out.println("Enter book ISBN: ");
+						String isbn = scnr.nextLine();
+						
+						System.out.println("Enter number of pages: ");
+						int pages = scnr.nextInt();
+						
+						bookInventory.addBook(new Book(id, title, author, isbn, pages));
+						break;
+					case 2:
+						System.out.println("Enter ID of book to borrow: ");
+						bookInventory.borrowBook(scnr.nextInt());
+						break;
+					case 3:
+						System.out.println("Enter ID of book to return: ");
+						bookInventory.returnBook(scnr.nextInt());
+						break;
+					case 4:
+						bookInventory.printAll();
+						break;
+					case 5:
+						System.out.println("Enter title of book: ");
+						bookInventory.searchBookTitle(scnr.nextLine());
+						break;
+					case 6:
+						System.out.println("Exiting program. Goodbye.");
+						break;
+					default: 
+						System.out.println("Please make a valid selection (1 - 6).");
+				}
+			} catch (Exception e) {
+				System.out.println("Invalid input. Please try again.");
+				scnr.nextLine();
+			}
 		}
 		scnr.close();
 	}
